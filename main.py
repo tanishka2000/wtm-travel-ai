0fb03a5d9cd68e40f8bb756d2a287a24f4ddc859
Elif Candan <115981542+encanmore@users.noreply.github.com>
2024-11-06 21:17:13 +0300

    Update main.py - import gemini & basic initial code

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+import google.generativeai as gemini
+import os
+
+os.environ['GOOGLE_API_KEY'] = "your-api-key-here"
+gemini.configure(api_key = os.environ['GOOGLE_API_KEY'])
+
+model = gemini.GenerativeModel('gemini-1.5-flash')
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/")
+def generateOutput(userInput):
+    responseData = model.generate_content(userInput)
+    return responseData
